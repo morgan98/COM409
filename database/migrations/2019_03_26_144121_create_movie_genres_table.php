b86a1e68c1b682e2bac7e4219a43bace7722e453
Declan O'Neill <45178209+decioneill@users.noreply.github.com>
2019-03-29 10:56:08 +0000

    small update
    testing branches

--- database/migrations/2019_03_26_144121_create_movie_genres_table.php
+++ database/migrations/2019_03_26_144121_create_movie_genres_table.php
@@ -20,6 +20,7 @@ class CreateMovieGenresTable extends Migration
             $table->timestamps();
         });
         Schema::table('movie_genres',function ($table) {
+            $table->unique(array('GenreID', 'productID'));
             $table->foreign('GenreID')->references('GenreID')->on('genres');
             $table->foreign('productID')->references('productID')->on('products');
         });
